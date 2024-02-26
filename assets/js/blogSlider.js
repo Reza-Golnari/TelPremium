721210e5c7ea7fa98b26bbf329e0baf8fa0b53ef
shayanHosseinChari <shayan001384@gmail.com>
2024-02-26 09:13:48 +0000

    improve(index.html): add phone numbers inside footer

--- assets/js/blogSlider.js
+++ assets/js/blogSlider.js
@@ -1,25 +1,30 @@
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  loop: true,
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 10,
+let wrappers = document.querySelectorAll('.mySwiper')
+wrappers.forEach(wrraper=>{
+
+  new Swiper(wrraper, {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      1000: {
+        slidesPerView: 4,
+      },
     },
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 1,
-      spaceBetween: 10,
-    },
-    // when window width is >= 640px
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 10,
-    },
-    1000: {
-      slidesPerView: 4.25,
-    },
-  },
-});
+  });
+})
+
